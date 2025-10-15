65dc7b066b75854836fff2f0989d28aa934f13d7
Asimjoiya4494 <asimashraf4494@gmail.com>
2025-10-15 10:56:28 +0500

    react app done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>My react App</h1>
+      <h2>Name: Muhammad Asim Ashraf</h2>
+      <p>About: I am learning DevOps and Web Development.</p>
+      <h3>Skills:</h3>
+      <ul style={{ listStyle: "none" }}>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>Git & GitHub</li>
+      </ul>
+      <p>Contact: asimashraf4494@example.com</p>
+    </div>
+  );
+}
+
+export default App;
